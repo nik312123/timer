4fec3417b0be5fbd89c7e49a67f00b11b04cabf3
Nikunj Chawla <nikchawla312@gmail.com>
2020-12-06 02:07:31 -0600

    Added the units of time returned

--- timer.ml
+++ timer.ml
@@ -4,7 +4,7 @@
 
 (**
     [time_exp_res] takes in a lazy expression, times how long it takes for the expression to run, and returns the
-    evaluated expression and the time it took to run
+    evaluated expression and the time it took to run in seconds
 *)
 let time_exp_res (exp_lazy: 'a lazy_t): 'a * float =
     let t_i = Sys.time () in
@@ -14,14 +14,14 @@ let time_exp_res (exp_lazy: 'a lazy_t): 'a * float =
 
 (**
     [time_exp] takes in a lazy expression, times how long it takes for the expression to run, and returns the time it
-    took to run
+    took to run in seconds
 *)
 let time_exp (exp_lazy: 'a lazy_t): float = time_exp_res exp_lazy |> snd
 
 (**
     [func_timer_res] takes in a lazy function and its input, times how long it takes for the function to run with the
-    given input, and returns the function result and the time it took to run; it may be useful to only provide the first
-    argument so that you can time the same function with various different inputs
+    given input, and returns the function result and the time it took to run in seconds; it may be useful to only
+    provide the first argument so that you can time the same function with various different inputs
 *)
 let func_timer_res (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): 'b * float =
     let t_i = Sys.time () in
@@ -31,7 +31,7 @@ let func_timer_res (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): 'b * float =
 
 (**
     [func_timer] takes in a lazy function and its input, times how long it takes for the function to run with the given
-    input, and returns the time it took to run; it may be useful to only provide the first argument so that you can time
-    the same function with various different inputs
+    input, and returns the time it took to run in seconds; it may be useful to only provide the first argument so that
+    you can time the same function with various different inputs
 *)
 let func_timer (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): float = func_timer_res fun_lazy input |> snd
