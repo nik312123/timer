6784ac2d986f63613da322ba8c7b751e90995a17
Nikunj Chawla <nikchawla312@gmail.com>
2020-12-15 07:33:52 -0600

    Modified timer.ml to use thunks instead of laziness for repeated timing

--- timer.ml
+++ timer.ml
@@ -3,72 +3,77 @@
 *)
 
 (**
-    [time_exp_res] takes in a lazy expression, times how long it takes for the expression to run, and returns the
-    evaluated expression and the time it took to run in seconds
-    @param exp_lazy The lazy expression to time
+    [time_exp_res] takes in an unevaluated expression using a thunk, times how long it takes for the expression to run,
+    and returns the evaluated expression and the time it took to run in seconds
+    @param exp_thunk The expression to time upon execution of the thunk
     @return A tuple containing the result of the expression and the time it took the expression to run in seconds
 *)
-let time_exp_res (exp_lazy: 'a lazy_t): 'a * float =
+let time_exp_res (exp_thunk: unit ->'a): 'a * float =
     let t_i = Sys.time () in
-    let res = Lazy.force exp_lazy in
+    let res = exp_thunk () in
     let t_f = Sys.time () in
     (res, t_f -. t_i)
 
 (**
-    [time_exp] takes in a lazy expression, times how long it takes for the expression to run, and returns the time it
-    took to run in seconds
-    @param exp_lazy The lazy expression to time
+    [time_exp] takes in an unevaluated expression using a thunk, times how long it takes for the expression to run, and
+    returns the time it took to run in seconds
+    @param exp_thunk The expression to time upon execution of the thunk
     @return The time it took the expression to run in seconds
 *)
-let time_exp (exp_lazy: 'a lazy_t): float = time_exp_res exp_lazy |> snd
+let time_exp (exp_thunk: unit -> 'a): float = time_exp_res exp_thunk |> snd
 
 (**
-    [time_exp_n] takes in a lazy expression, times how long it takes for the expression to run [n] times, and returns
-    the time it took to run in seconds
-    @param exp_lazy The lazy expression to time
+    [time_exp_n] takes in an unevaluated expression using a thunk, times how long it takes for the expression to run
+    [n] times, and returns the time it took to run in seconds
+    @param n         The number of times to test the expression
+    @param exp_thunk The expression to time upon execution of the thunk
     @return The time it took the expression to run [n] times in seconds
 *)
-let time_exp_n (n: int) (exp_lazy: 'a lazy_t): float =
+let time_exp_n (n: int) (exp_thunk: unit -> 'a): float =
     let rec time_exp_n_aux (i: int) (acc: float): float =
         if i <= 0 then acc
-        else time_exp_n_aux (i - 1) (acc +. (time_exp exp_lazy))
+        else time_exp_n_aux (i - 1) (acc +. (time_exp exp_thunk))
     in time_exp_n_aux n 0.
 
 (**
-    [func_timer_res] takes in a lazy function and its input, times how long it takes for the function to run with the
-    given input, and returns the function result and the time it took to run in seconds; it may be useful to only
-    provide the first argument so that you can time the same function with various different inputs
-    @param fun_lazy The lazy function to time with [input]
-    @param input    The input to the function to time
+    [func_timer_res] takes in an unevaluated function using a thunk and its unevaluated input using a thunk, times how
+    long it takes for the function to run with the given input, and returns the function result and the time it took to
+    run in seconds; it may be useful to only provide the first argument so that you can time the same function with
+    various different inputs
+    @param fun_thunk   The function thunk to time with [input_thunk] upon execution of the thunk
+    @param input_thunk The input to the function to time that will be evaluated upon execution of the thunk
     @return A tuple containing the output of the function and the time it took the function to run in seconds with the
     given input
 *)
-let func_timer_res (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): 'b * float =
+let func_timer_res (fun_thunk: unit -> ('a -> 'b)) (input_thunk: unit -> 'a): 'b * float =
     let t_i = Sys.time () in
-    let res = input |> Lazy.force fun_lazy in
+    let res = input_thunk () |> fun_thunk () in
     let t_f = Sys.time () in
     (res, t_f -. t_i)
 
 (**
-    [func_timer] takes in a lazy function and its input, times how long it takes for the function to run with the given
-    input, and returns the time it took to run in seconds; it may be useful to only provide the first argument so that
-    you can time the same function with various different inputs
-    @param fun_lazy The lazy function to time with [input]
-    @param input    The input to the function to time
+    [func_timer] takes in an unevaluated function using a thunk and its unevaluated input using a thunk, times how long
+    it takes for the function to run with the given input, and returns the time it took to run in seconds; it may be
+    useful to only provide the first argument so that you can time the same function with various different inputs
+    @param fun_thunk   The function thunk to time with [input_thunk] upon execution of the thunk
+    @param input_thunk The input to the function to time that will be evaluated upon execution of the thunk
     @return The time it took the function to run in seconds with the given input
 *)
-let func_timer (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): float = func_timer_res fun_lazy input |> snd
+let func_timer (fun_thunk: unit -> ('a -> 'b)) (input_thunk: unit -> 'a): float =
+    func_timer_res fun_thunk input_thunk |> snd
 
 (**
-    [func_timer] takes in a lazy function and its input, times how long it takes for the function to run with the given
-    input [n] times, and returns the time it took to run in seconds; it may be useful to only provide the first argument
-    so that you can time the same function with various different inputs
-    @param fun_lazy The lazy function to time with [input]
-    @param input    The input to the function to time
+    [func_timer] takes in an unevaluated function using a thunk and its unevaluated input using a thunk, times how long
+    it takes for the function to run with the given input [n] times, and returns the time it took to run in seconds; it
+    may be useful to only provide the first argument so that you can time the same function with various different
+    inputs
+    @param n           The number of times to test the function with the given input
+    @param fun_thunk   The function thunk to time with [input_thunk] upon execution of the thunk
+    @param input_thunk The input to the function to time that will be evaluated upon execution of the thunk
     @return The time it took the function to run [n] times in seconds with the given input
 *)
-let func_timer_n (n: int) (fun_lazy: ('a -> 'b) lazy_t) (input: 'a): float =
+let func_timer_n (n: int) (fun_thunk: unit -> ('a -> 'b)) (input_thunk: unit -> 'a): float =
     let rec func_timer_n_aux (i: int) (acc: float): float =
         if i <= 0 then acc
-        else func_timer_n_aux (i - 1) (acc +. (input |> Lazy.force fun_lazy))
+        else func_timer_n_aux (i - 1) (acc +. (func_timer fun_thunk input_thunk))
     in func_timer_n_aux n 0.
